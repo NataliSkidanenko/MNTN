e59870f38004f4d201bbe8b233c07065bb877007
NataliSkidanenko <sni.nata102@gmail.com>
2023-01-03 16:48:49 +0100

    Burger menu

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,43 @@
-{
-    const modalMenu = document.querySelector('.modal-block');
-    const buttonOpenMenu = document.querySelector('#menu-open');
-    const buttonCloseMenu = document.querySelector('#menu-close');
+const modalMenu = document.querySelector('.modal-block');
+const buttonOpenMenu = document.querySelector('#menu-open');
+const buttonCloseMenu = document.querySelector('#menu-close');
 
-    buttonOpenMenu.addEventListener('click', openMenu);
-    buttonCloseMenu.addEventListener('click', closeMenu);
+buttonOpenMenu.onclick = function (e) {
+    e.preventDefault();
+    modalMenu.classList.add('animation-in');
+    modalMenu.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+};
 
-    function openMenu() {
-        modalMenu.classList.add('animation-in');
-        modalMenu.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-        return false;
-    }
+buttonCloseMenu.onclick = function (e) {
+    e.preventDefault();
+    modalMenu.classList.remove('animation-in');
+    modalMenu.classList.add('animation-out');
 
-    function closeMenu() {
-        modalMenu.classList.remove('animation-in');
-        modalMenu.classList.add('animation-out');
+    setTimeout(() => {
+        modalMenu.classList.remove('animation-out');
+        modalMenu.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }, 500);
+};
+// buttonOpenMenu.addEventListener('click', openMenu);
+// buttonCloseMenu.addEventListener('click', closeMenu);
 
-        setTimeout(() => {
-            modalMenu.classList.remove('animation-out');
-            modalMenu.style.display = 'none';
-            document.body.style.overflow = 'auto';
-        }, 500);
-        return false;
-    }
-}
+// function openMenu(e) {
+//     e.preventDefault();
+//     modalMenu.classList.add('animation-in');
+//     modalMenu.style.display = 'flex';
+//     document.body.style.overflow = 'hidden';
+// }
+
+// function closeMenu(e) {
+//     e.preventDefault();
+//     modalMenu.classList.remove('animation-in');
+//     modalMenu.classList.add('animation-out');
+
+//     setTimeout(() => {
+//         modalMenu.classList.remove('animation-out');
+//         modalMenu.style.display = 'none';
+//         document.body.style.overflow = 'auto';
+//     }, 500);
+// }
